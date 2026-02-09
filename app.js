71470368bbb795458167f6c8148e6c068eca5a37
Nishchal Raj Subedi <111012172+RazNishchal@users.noreply.github.com>
2026-02-09 23:10:08 +0545

    updated

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const btn = document.querySelector("form button");
 const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
 const msg = document.querySelector(".msg");
-
+//okay
 for (let select of dropdowns) {
     for (currCode in countryList) {
         let newOption = document.createElement("option");
